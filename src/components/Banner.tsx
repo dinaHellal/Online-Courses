350641c9ea4f89f55ee082d2587e58b948081e0b
dina hellal <dinahellal12@gmail.com>
2025-05-24 15:06:54 +0200

    Start: React Project & Add: Banner Section

---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+import styleCss from "./banner.module.css";
+export default function Banner(){
+return ( 
+  <div className="banner">
+
+  <p className={styleCss.title}>Free Courses 🌟 Sale Ends Soon, Get It Now</p>
+<img src="/Icon (14).png" alt="Icon" />
+  </div>
+);
+}
