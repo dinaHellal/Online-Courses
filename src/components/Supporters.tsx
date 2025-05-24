cbf4030e0c310952313b355132eb8cc06f4a1b59
dina hellal <dinahellal12@gmail.com>
2025-05-24 20:11:26 +0200

    Edit: Icon Src In Supporter Component

--- src/components/Supporters.tsx
+++ src/components/Supporters.tsx
@@ -4,12 +4,12 @@ export default function Supporter(){
   <>
   <div className={supportStyle.support}>
   <div className={supportStyle.supporter}>    
-<img src="/public/Card.png" alt="Card" />
-<img src="/public/Card (1).png" alt="Card" />
-<img src="/public/Card (2).png" alt="Card" />
-<img src="/public/Card (3).png" alt="Card" />
-<img src="/public/Card (4).png" alt="Card" />
-<img src="/public/Card (5).png" alt="Card" />
+<img src="/Card.png" alt="Card" />
+<img src="/Card (1).png" alt="Card" />
+<img src="/Card (2).png" alt="Card" />
+<img src="/Card (3).png" alt="Card" />
+<img src="/Card (4).png" alt="Card" />
+<img src="/Card (5).png" alt="Card" />
 </div>
   </div>
   </>
